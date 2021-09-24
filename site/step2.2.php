8bc30408e12df0d6b9bdf4109a248af060fab641
Camilo Loaiza Piedrahita <camilo.loaizapiedrahita@dawsoncollege.qc.ca>
2021-09-24 11:16:22 -0400

    Create step2.2.php

---
+++ site/step2.2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Step 2.2</title>
+        <body>
+            <p>
+                <?= var_dump($_SERVER) ?>
+            </p>
+        </body>
+    </head>     
+</html>
