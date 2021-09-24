e89815c0e4033006719e257f7ef3f0d9b1676a77
Camilo Loaiza Piedrahita <camilo.loaizapiedrahita@dawsoncollege.qc.ca>
2021-09-24 11:46:21 -0400

    step 3.2

---
+++ site/sub-pages/formresults.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>formresults.php</title>
+    </head>
+    <body> 
+        <p>
+            <?= var_dump($_GET) ?>
+        </p>
+        <p>
+            <?= var_dump($_POST) ?>
+        </p>
+    </body>
+</html>
