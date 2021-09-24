1af5841b7b0c2b20e49cf282ee2d165d3ba88245
Camilo Loaiza Piedrahita <camilo.loaizapiedrahita@dawsoncollege.qc.ca>
2021-09-24 12:39:50 -0400

    step 3.5 test

--- site/sub-pages/formresults.php
+++ site/sub-pages/formresults.php
@@ -4,6 +4,21 @@
         <title>formresults.php</title>
     </head>
     <body> 
+        <p> The two founders of The Beatles are:
+            <?php
+                if(isset($_POST['member1']))
+                echo "Paul"<br>
+
+                if(isset($_POST['member2']))
+                echo "Ringo"<br>
+
+                if(isset($_POST['member3']))
+                echo "John"<br>
+
+                if(isset($_POST['member4']))
+                echo "George"<br>
+            ?>
+        </p>
         <p>
             John Lennon died in <?= (int)$_POST['year']; ?> 
         </p>
