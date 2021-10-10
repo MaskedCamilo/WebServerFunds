3939b940775b5804296d13719620e2f023973758
MaskedCamilo <camilo0599@gmail.com>
2021-10-10 12:48:56 -0400

    Update formresults.php

--- site/sub-pages/formresults.php
+++ site/sub-pages/formresults.php
@@ -75,8 +75,7 @@
             $result = mysqli_query($conn, $sql);
             foreach($result as $row) {echo "Album Number: {$row['id']} | Album Name: 
                 {$row['album_name']} | Release Year: {$row['release_year']} | Length: 
-                {$row['length']} | First Single: {$row['first_single']} <br/>";}  
-            echo "<br/>"; 
+                {$row['length']} | First Single: {$row['first_single']} <br/><br/>";}  
 
             $album = $_POST['album'];
             $ryear = $_POST['ryear'];
