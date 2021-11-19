8e0fbc08d661536988c4117502af727708376b6a
Camilo Loaiza Piedrahita <camilo.loaizapiedrahita@dawsoncollege.qc.ca>
2021-11-19 12:19:42 -0500

    lab11

---
+++ site/temperature.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>temperature.php</title>
+    </head>
+    <body>
+        <p>
+            <?php
+                $raw = `../bme280`; 
+                echo $raw;
+                $deserialized = json_decode($raw);
+                var_dump($deserialized);
+                echo "Temperature: {$deserialized->temperature}, Pressure: {$deserialized->pressure},
+                Altitude: {$deserialized->altitude}";
+            ?>
+        </p>
+    </body>
+</html>
