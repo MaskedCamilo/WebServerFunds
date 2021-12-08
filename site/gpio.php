99791af54fcf3b17a7d6c07ffab054a987c49afd
MaskedCamilo <camilo0599@gmail.com>
2021-12-08 00:30:40 -0500

    PBL test

---
+++ site/gpio.php
@@ -0,0 +1,11 @@
+<?php
+	$output = gpio read 1;
+		if( $output == '1')
+		      {
+			echo "The logic value of GPIO pin 18 is 1";
+		      }
+		else
+		      {
+			echo "The logic value of GPIO pin 18 is 0";
+		      }
+?>
--- site/temperature.php
+++ site/temperature.php
@@ -11,6 +11,21 @@
                 echo "Temperature: {$deserialized->temperature}, Pressure: {$deserialized->pressure},
                 Altitude: {$deserialized->altitude}"; //Displays the value of the variable
             ?>
-        </p>
+        </p>Click the following button to display the value of GPIO Pin 1:
+        <button id="gpio" type="button" onclick= "dis_gpio()">GPIO Value</button>
+
+        <script>
+            //AJAX application that asynchronously retrieves and displays contents of the pm_firstsongs.txt file
+            function dis_gpio() {
+                var xhttp = new XMLHttpRequest();   //Creates a XMLHttpRequest object
+                xhttp.onload = function() {         //Define a callback function
+                    document.getElementById("gpio_value").innerHTML =
+                    this.responseText;
+                }
+            xhttp.open("GET", "gpio.php"); //Sends a request
+            xhttp.send();
+            }
+        </script> 
+        <p id="gpio_value"></p>
     </body>
 </html>
