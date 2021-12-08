b95f31029860340eea8d964a6e1337beb0133eeb
MaskedCamilo <camilo0599@gmail.com>
2021-12-08 01:07:05 -0500

    Update gpio.php

--- site/gpio.php
+++ site/gpio.php
@@ -1,13 +1,13 @@
 <?php
 	echo 'its working';
 
-	//$output = gpio read 1;
-	//if( $output == '1')
-	//{
-	  //echo "The logic value of GPIO pin 18 is 1";
-	//}
-	///else
-	//{
-	  //echo "The logic value of GPIO pin 18 is 0";
-	//}
+	$output = `gpio read 1`;
+	if( $output == '1')
+	{
+	  echo 'The logic value of GPIO pin 18 is 1';
+	}
+	else
+	{
+	  echo 'The logic value of GPIO pin 18 is 0';
+	}
 ?>
