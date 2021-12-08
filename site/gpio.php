33f1f2f7822fcd629ee859aacec26c89a4e91e8a
MaskedCamilo <camilo0599@gmail.com>
2021-12-08 00:55:37 -0500

    Update gpio.php

--- site/gpio.php
+++ site/gpio.php
@@ -1,3 +1,4 @@
-<?php
-	echo 'test';
-?>
+<p>Other early songs composed by Paul McCartney are:</br>
+-Suicide (1956)</br>
+-When I'm Sixty Four (Started being written in 1956 and
+officially released in 1967)</p>
