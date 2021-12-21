94b2c86215def94ea3adcdeec30224fc7da848a6
Camilo Loaiza Piedrahita <camilo.loaizapiedrahita@dawsoncollege.qc.ca>
2021-12-21 12:02:54 -0500

    Update g00gieres.php

--- final/g00gieres.php
+++ final/g00gieres.php
@@ -7,12 +7,9 @@
         <p>
             <?php
                 $sterm = $_POST['search'];
-                echo $sterm;
-
                 $ipadr = $_SERVER['REMOTE_ADDR'];
-                echo $ipadr;
 
-                //header("Location: https://www.google.ca/search?q=$sterm"); 
+                header("Location: https://www.google.ca/search?q=$sterm"); 
 
               //Establishes connection to database
               $server = "localhost";
@@ -21,12 +18,10 @@
               $database = "albums";
               $conn = mysqli_connect($server, $username, $password, $database);
 
+              //Inserts data into the database
               $sql = "INSERT INTO search (search_term, ip_address)
               VALUES ('$sterm', '$ipadr')";
               $result = mysqli_query($conn, $sql);
-              echo $result ? "The album was sucessfully submitted!" 
-              : "Could not submit album, try again.: " . mysqli_error($conn);
-
               mysqli_close($conn);
 
             ?>        
