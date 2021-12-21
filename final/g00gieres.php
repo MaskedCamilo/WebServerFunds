01fd5112f18b1eb484364d3fa4051a980de895d4
Camilo Loaiza Piedrahita <camilo.loaizapiedrahita@dawsoncollege.qc.ca>
2021-12-21 11:12:08 -0500

    Final

---
+++ final/g00gieres.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>g00gieres.php</title>
+    </head>
+    <body> 
+        <p>The search was:<br>
+            <?php
+                $_POST('search');
+            ?>
+        </p>
+    </body>
+</hmtl>
